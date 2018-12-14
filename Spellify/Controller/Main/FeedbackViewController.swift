06b031b526c65fd183a3d727b7d830010a7ab658
oslover21 <lijin_it@live.com>
2018-12-13 23:22:23 -0700

    init controllers

---
+++ Spellify/Controller/Main/FeedbackViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedbackViewController.swift
+//  Spellify
+//
+//  Created by Macmini on 12/13/18.
+//  Copyright © 2018 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
