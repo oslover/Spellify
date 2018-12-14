7e2600fe1237b4ca01735032edd05a784a5d4a0b
oslover21 <lijin_it@live.com>
2018-12-13 23:10:32 -0700

    Initial Commit

---
+++ Spellify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spellify
+//
+//  Created by Macmini on 12/13/18.
+//  Copyright © 2018 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
